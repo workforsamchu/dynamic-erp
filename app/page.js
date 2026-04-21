80cc714fba63dc4830dbd19dedf5423b2a901f54
Sam Chu <sam.chu.cbxsoftware@gmail.com>
2026-04-20 23:14:44 -0700

    edit record

--- app/page.js
+++ app/page.js
@@ -1,11 +1,37 @@
-import DynamicForm from "@/components/DynamicForm";
+"use client"
+
+import { useState } from "react"
+import DynamicForm from "@/components/DynamicForm"
+import RecordsTable from "@/components/RecordsTable"
 
 export default function Page() {
+    const recordTypeId = "69e3650ea24960288a672039"
+
+    const [refreshKey, setRefreshKey] = useState(0)
+    const [selectedRecord, setSelectedRecord] = useState(null)
+
+    function handleSuccess() {
+        setRefreshKey((prev) => prev + 1)
+        setSelectedRecord(null) // 🔥 清 edit mode
+    }
+
     return (
         <div>
             <h1>Record System</h1>
 
-            <DynamicForm recordTypeId="69e3650ea24960288a672039" />
+            <DynamicForm
+                recordTypeId={recordTypeId}
+                onSuccess={handleSuccess}
+                selectedRecord={selectedRecord} // 🔥 新增
+            />
+
+            <hr />
+
+            <RecordsTable
+                recordTypeId={recordTypeId}
+                refreshKey={refreshKey}
+                onRowClick={setSelectedRecord} // 🔥 點 row
+            />
         </div>
-    );
+    )
 }
